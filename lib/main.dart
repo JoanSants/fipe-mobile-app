6cad6cac9b9404353ab171ed0b8ea00557a6607a
JoanSantsValid <jsantos.5a@ext.valid.com>
2021-03-25 15:31:37 -0300

    feat: Handle vehicle models route

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:fipe_mobile_app/pages/home.dart';
 import 'package:fipe_mobile_app/pages/brands.dart';
+import 'package:fipe_mobile_app/pages/vehicle-models.dart';
 
 void main() {
   runApp(MaterialApp(
     initialRoute: '/',
     routes: {
       '/': (context) => Home(),
-      '/brands': (context) => Brands()
+      '/brands': (context) => Brands(),
+      '/models': (context) => VehicleModels()
     },
   ));
 }

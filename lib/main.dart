6f1c58173d248a90c9ce85041e79de5dd5b91c8f
JoanSantsValid <jsantos.5a@ext.valid.com>
2021-03-25 11:41:26 -0300

    feat: Set Brand page route

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:fipe_mobile_app/pages/home.dart';
+import 'package:fipe_mobile_app/pages/brands.dart';
 
 void main() {
   runApp(MaterialApp(
     initialRoute: '/',
     routes: {
-      '/': (context) => Home()
+      '/': (context) => Home(),
+      '/brands': (context) => Brands()
     },
   ));
 }

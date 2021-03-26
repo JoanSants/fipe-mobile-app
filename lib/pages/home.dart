549f5d28d3aabc886aa60a54dde4a28649956dba
JoanSantsValid <jsantos.5a@ext.valid.com>
2021-03-25 22:51:33 -0300

    chore: Format home page code

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,7 @@ class _HomeState extends State<Home> {
   List<String> vehicleTypes = ['carros', 'motos'];
   String selectedVehicleType = '';
 
-  void fetchBrands () async {
+  void fetchBrands() async {
     Fipe fipe = new Fipe(vehicleType: selectedVehicleType);
     await fipe.fetchBrands();
     if (fipe.brands.length > 0) {
@@ -29,9 +29,7 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         title: Text(
           'Veículos'.toUpperCase(),
-          style: TextStyle(
-              letterSpacing: 8
-          ),
+          style: TextStyle(letterSpacing: 8),
         ),
       ),
       body: Padding(
@@ -42,21 +40,24 @@ class _HomeState extends State<Home> {
             Text('Selecione o tipo de veículo:'.toUpperCase()),
             SizedBox(width: 10),
             DropdownButton<String>(
-                value: selectedVehicleType.isNotEmpty ? selectedVehicleType : 'carros',
+                value: selectedVehicleType.isNotEmpty
+                    ? selectedVehicleType
+                    : 'carros',
                 onChanged: (String newValue) => {
-                  setState(() => {
-                    selectedVehicleType = newValue
-                  })
-                },
-                items: vehicleTypes.map<DropdownMenuItem<String>>((String vehicleType) => (
-                    DropdownMenuItem<String>(
-                      child: Text(vehicleType.toUpperCase()),
-                      value: vehicleType,
-                    )
-                )).toList()
-            ),
+                      setState(() => {selectedVehicleType = newValue})
+                    },
+                items: vehicleTypes
+                    .map<DropdownMenuItem<String>>(
+                        (String vehicleType) => (DropdownMenuItem<String>(
+                              child: Text(vehicleType.toUpperCase()),
+                              value: vehicleType,
+                            )))
+                    .toList()),
             SizedBox(height: 20),
-            Button(disabled: selectedVehicleType.isEmpty, label: 'Buscar Marcas', onPressed: fetchBrands)
+            Button(
+                disabled: selectedVehicleType.isEmpty,
+                label: 'Buscar Marcas',
+                onPressed: fetchBrands)
           ],
         ),
       ),
